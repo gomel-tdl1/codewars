a5fc73276ba82355080c3b387a8717ef1aef4087
Ilya Taldykin <rock_cell@mail.ru>
2020-04-12 23:01:17 +0300

    codeWars projects

--- training.js
+++ training.js
@@ -204,28 +204,240 @@
 // console.log(now);
 
 //--------------------------------------------------------------------------------------------
+//-----------------------PROTOTYPES
+// function Dog(name, breed, weight) {
+//     this.name = name;
+//     this.breed = breed;
+//     this.weight = weight;
+// }
+// Dog.prototype.species = "Canine";
+// Dog.prototype.bark = function() {
+//     if (this.weight > 25) {
+//         console.log(this.name + " says Woof!");
+//     } else {
+//         console.log(this.name + " says Yip!");
+//     }
+// };
+// Dog.prototype.run = function() {
+//     console.log("Run!");
+// };
+// Dog.prototype.wag = function() {
+//     console.log("Wag!");
+// };
+// let fido = new Dog("Fido", "Mixed", 38);
+// let fluffy = new Dog("Fluffy", "Poodle", 30);
+// let spot = new Dog("Spot", "Chihuahua", 10);
+// fido.bark();
+// fido.run();
+// fido.wag();
+// fluffy.bark();
+// fluffy.run();
+// fluffy.wag();
+// spot.bark();
+// spot.run();
+// spot.wag();
 
-function carOfMyDream() {
-    this.make = "Webville Motors";
-    this.year=2020;
-    this.started = false;
-    this.start = function () {
-        this.started = true;
-    };
-    this.stop = function () {
-        this.started = false;
-    };
-    this.drive = function () {
-        if (this.started) {
-            console.log(this.make + " " +
-                this.model + " goes zoom zoom!");
-        } else {
-            console.log("Start the engine first.");
-        }
-    };
-}
-let taxi=new carOfMyDream();
-taxi.model="Bullet";
-taxi.upDoor=true;
-taxi.streamlinedForm=true;
-taxi.diameterWheels=20;
+//--------------------------------------------------------------------------------------------
+
+// function СarOfMyDream() {
+//     this.make = "Webville Motors";
+//     this.year=2020;
+//     this.started = false;
+//     this.start = function () {
+//         this.started = true;
+//     };
+//     this.stop = function () {
+//         this.started = false;
+//     };
+//     this.drive = function () {
+//         if (this.started) {
+//             console.log(this.make + " " +
+//                 this.model + " goes zoom zoom!");
+//         } else {
+//             console.log("Start the engine first.");
+//         }
+//     };
+// }
+// let taxi=new СarOfMyDream();
+// taxi.model="Bullet";
+// taxi.upDoor=true;
+// taxi.streamlinedForm=true;
+// taxi.diameterWheels=20;
+
+//---------------------------------------------------------------------------------------------
+
+// function Robot(name,year,owner) {
+//     this.name=name;
+//     this.year=year;
+//     this.owner=owner;
+// }
+// Robot.prototype.maker="fabric";
+// Robot.prototype.speak=function () {
+//     console.log(this.name+" is Speaking");
+// };
+// Robot.prototype.makeCoffee=function () {
+//     console.log(this.name+" make coffee for "+this.owner);
+// };
+// Robot.prototype.blinkLights=function () {
+//     console.log(this.name+" blinks light 4 times");
+// };
+//
+// let robby=new Robot("Robby",1999,"Mike");
+// let rosie=new Robot("Rosie",1956,"Linda");
+//
+// robby.onOffSwitch=true;
+// robby.makeCoffee=function () {
+//     console.log(this.name+" brought coffee from Starbucks for "+this.owner);
+// };
+//
+// rosie.cleanHouse=function () {
+//     console.log(this.name+" is cleaning house");
+// };
+// console.log(robby.name + " was made by " + robby.maker +
+//     " in " + robby.year + " and is owned by " + robby.owner);
+// robby.makeCoffee();
+// robby.blinkLights();
+// console.log(rosie.name + " was made by " + rosie.maker +
+//     " in " + rosie.year + " and is owned by " + rosie.owner);
+// rosie.cleanHouse();
+
+//----------------------------------------------------------------------------
+
+// function Game() {
+//     this.level = 0;
+// }
+// Game.prototype.play = function() {
+// // Действия игрока
+//     this.level++;
+//     console.log("Welcome to level " + this.level);
+//     this.unlock();
+// };
+// Game.prototype.unlock = function() {
+//     if(this.level===42){
+//         return " is blasting you with laser beams.";
+//     }
+// };
+// function Robot(name, year, owner) {
+//     this.name = name;
+//     this.year = year;
+//     this.owner = owner;
+// }
+// Robot.prototype.deployLaser=function(){
+//   console.log(this.name+game.unlock()) ;
+// };
+// let game = new Game();
+// let robby = new Robot("Robby", 1956, "Dr. Morbius");
+// let rosie = new Robot("Rosie", 1962, "George Jetson");
+// while (game.level < 42) {
+//     game.play();
+// };
+// robby.deployLaser();
+// rosie.deployLaser();
+
+//----------------------------------------------------------------------------------------
+
+// function Dog(name, breed, weight) {
+//     this.name = name;
+//     this.breed = breed;
+//     this.weight = weight;
+// }
+//
+// Dog.prototype.species = "Canine";
+// Dog.prototype.bark = function () {
+//     if (this.weight > 25) {
+//         console.log(this.name + " says Woof!");
+//     } else {
+//         console.log(this.name + " says Yip!");
+//     }
+// };
+// Dog.prototype.run = function () {
+//     console.log("Run!");
+// };
+// Dog.prototype.wag = function () {
+//     console.log("Wag!");
+// };
+//
+// function ShowDog(name, breed, weight, handler) {
+//     Dog.call(this, name, breed, weight);
+//     this.handler = handler;
+// }
+//
+// ShowDog.prototype = new Dog();//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// ShowDog.prototype.constructor = ShowDog;
+// ShowDog.prototype.league = "Webville";
+// ShowDog.prototype.stack = function () {
+//     console.log("Stack");
+// };
+// ShowDog.prototype.bait = function () {
+//     console.log("Bait");
+// };
+// ShowDog.prototype.gait = function (kind) {
+//     console.log(kind + "ing");
+// };
+// ShowDog.prototype.groom = function () {
+//     console.log("Groom");
+// };
+//
+// let fido = new Dog("Fido", "Mixed", 38);
+// let fluffy = new Dog("Fluffy", "Poodle", 30);
+// let spot = new Dog("Spot", "Chihuahua", 10);
+// let scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+// let beatrice = new ShowDog("Beatrice", "Pomeranian", 5, "Hamilton");
+// fido.bark();
+// fluffy.bark();
+// spot.bark();
+// scotty.bark();
+// beatrice.bark();
+// scotty.gait("Walk");
+// beatrice.groom();
+
+//-----------------------------------------------------------------------------------------------------
+
+// function Robot(name,year,owner) {
+//     this.name=name;
+//     this.year=year;
+//     this.owner=owner;
+// }
+// Robot.prototype.maker="fabric";
+// Robot.prototype.speak=function () {
+//     console.log(this.name+" is Speaking");
+// };
+// Robot.prototype.makeCoffee=function () {
+//     console.log(this.name+" make coffee for "+this.owner);
+// };
+// Robot.prototype.blinkLights=function () {
+//     console.log(this.name+" blinks light 4 times");
+// };
+//
+// function SpaceRobot(name, year, owner, homePlanet) {
+//     Robot.call(this, name,year,owner);
+//     this.homePlanet=homePlanet;
+// }
+// SpaceRobot.prototype = new Robot();
+// SpaceRobot.prototype.constructor=SpaceRobot;
+// SpaceRobot.prototype.speak = function() {
+//     alert(this.name + " says Sir, If I may venture an opinion...");
+// };
+// SpaceRobot.prototype.pilot = function() {
+//     alert(this.name + " says Thrusters? Are they important?");
+// };
+// let c3po = new SpaceRobot("C3PO", 1977, "Luke Skywalker", "Tatooine");
+// c3po.speak();
+// c3po.pilot();
+// console.log(c3po.name + " was made by " + c3po.maker);
+// let simon = new SpaceRobot("Simon", 2009, "Carla Diana", "Earth");
+// simon.makeCoffee();
+// simon.blinkLights();
+// simon.speak();
+
+//-----------------------------------------------------------------------------------------------
+
+String.prototype.palindrome=function () {
+    if(this.toString()===this.split("").reverse().join("")){
+        return true;
+    }else{
+        return false;
+    }
+};
+console.log("polop".palindrome());
+console.log("polopl".palindrome());
